669e36aa09594facc13aa5e9d2cba52ccde32ac4
GoMotiv <newage3333@gmail.com>
2019-03-14 13:41:54 +0900

    asdfasdf
    asdfasdf

--- matrix.cpp
+++ matrix.cpp
@@ -27,6 +27,7 @@ void random_walk(int(*a)[COLS], int rows) /// this function assign values to 2d 
 			a[r][c] = rand() % 1000;			//// assign any random number between 0 and 1000 using rand() function
 }
 
+//test
 void show(int *a, int rows, int cols) //// this function shows all the elements in a 2d Array
 {
 
